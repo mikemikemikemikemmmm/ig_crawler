354da17b38a68b07a28a6a6968a9588b50d0618b
Your Name <you@example.com>
2025-07-30 19:27:48 +0800

    add print

--- src/handler.rs
+++ src/handler.rs
@@ -27,17 +27,26 @@ impl IntoResponse for HandlerError {
 pub async fn get_ig_detail(
     Path(user_name): Path<String>,
 ) -> Result<Json<_struct::count::User>, HandlerError> {
+    println!("req user name : {}", &user_name);
     if user_name.len() > 40 {
         return Err(HandlerError::new("username is too long"));
     }
     let user_pk = fetch::pk::fetch_ig_to_get_user_pk(&user_name)
         .await
-        .map_err(|e| HandlerError::new(e))?;
+        .map_err(|e| {
+            println!("fetch user pk failed.");
+            HandlerError::new(e)
+        })?;
+    println!("req user pk : {}", &user_pk);
     let user_data = fetch::user_data::fetch_ig_to_get_user_data(user_pk)
         .await
         .map_err(|e| {
+            println!("fetch user data failed.");
             HandlerError::new(e)
         })?;
+    println!("req user media count : {}", &user_data.media_count);
+    println!("req user follower_count : {}", &user_data.follower_count);
+    println!("req user following_count : {}", &user_data.following_count);
     // let user_data: _struct::count::User = _struct::count::User {
     //     follower_count: 1,
     //     following_count: 2,
